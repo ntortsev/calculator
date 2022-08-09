a7b959fbcd22fe77cb34d48cabe9f1e0ce4e2828
Nikita Tortsev <nikitatortsev@gmail.com>
2022-08-09 12:58:24 +0500

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const arrButtons = [
+  'C',
+  '()',
+  '%',
+  '/',
+  '7',
+  '8',
+  '9',
+  'x',
+  '4',
+  '5',
+  '6',
+  '-',
+  '1',
+  '2',
+  '3',
+  '+',
+  '+/-',
+  '0',
+  '.',
+  '=',
+];
 
 function App() {
+  const [inputValue, setInputValue] = React.useState('');
+
+  const onClickButton = (btn: string) => {
+    setInputValue(inputValue + btn);
+
+    switch (btn) {
+      case '+':
+        return console.log('плюс');
+      case '-':
+        return console.log('минус');
+      case 'x':
+        return console.log('умножить');
+      case '/':
+        return console.log('разделить');
+      case '=':
+        return console.log('равно');
+      case '%':
+        return console.log('процент');
+      case 'C':
+        return setInputValue('');
+      case '.':
+        return console.log('дробное число');
+      case '+/-':
+        return console.log('смена знака');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="wrapper__window">
+        <input
+          value={inputValue}
+          onChange={(event) => setInputValue(event.target.value)}
+          className="window__input--top"
+          type="text"
+        />
+        <input className="window__input--bottom" type="text" disabled />
+      </div>
+      <div className="wrapper__buttons">
+        {arrButtons.map((btn, index) => {
+          return (
+            <button onClick={() => onClickButton(btn)} key={index} className="button">
+              {btn}
+            </button>
+          );
+        })}
+      </div>
     </div>
   );
 }
